dd1590dad0e7637c6ff3287229b3e0117f3874a7
Seungwon <dnjs5397@naver.com>
2022-04-10 17:52:29 +0900

    2022-04-10 practice

---
+++ 위장.py
@@ -0,0 +1,17 @@
+clothes = [["yellowhat", "headgear"], 
+           ["bluesunglasses", "eyewear"], 
+           ["green_turban", "headgear"]]
+
+ha = {}
+
+for i in clothes:
+    if i[1] in ha:
+        ha[i[1]] += 1
+    else:
+        ha[i[1]] = 1
+answer = 1
+for i in ha:
+    answer *= (ha[i]+1)
+    
+print(answer-1)
+
