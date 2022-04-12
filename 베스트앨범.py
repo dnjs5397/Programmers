8dd819e73a2e589d37eee6bb757e1ee40ff1c480
Seungwon <dnjs5397@naver.com>
2022-04-12 23:37:59 +0900

    2022-04-12 practice

--- 베스트앨범.py
+++ 베스트앨범.py
@@ -1,6 +1,6 @@
-genres = ["classic", "pop", "classic", "classic", "pop"]
+genres = ["classic", "pop", "classic", "classic", "pop", "test"]
 
-plays = [500, 600, 150, 800, 2500]
+plays = [500, 600, 150, 800, 2500, 100]
 
 answer = []
 g1 = {}
@@ -11,8 +11,10 @@ for i in range(len(plays)):
         g1[genres[i]] = 0
         g1[genres[i]] += plays[i]
 
-s_genre = sorted(g1.items(), reverse=True)
+s_genre = sorted(g1.items())
+s_genre.sort(key = lambda x : x[1], reverse=True)
 
+print(s_genre)
 g2 = [ [] for _ in range(len(s_genre)) ]
 for i in range(len(s_genre)):
     for j in range(len(plays)):
@@ -20,17 +22,14 @@ for i in range(len(s_genre)):
             g2[i].append((j, plays[j]))
             
 for i in g2:
-    i = i.sort(key = lambda x : x[1], reverse=True)
+    i.sort(key = lambda x : x[1], reverse=True)
     
+print(g2)
 for i in g2:
     if len(i) == 1:
         answer.append(i[0][0])
-    elif len(i)%2 == 0:
-        for j in i:
-            answer.append(j[0])
-    elif len(i)%2 == 1:
-        i.pop()
-        for j in i:
-            answer.append(j[0])
+    else:
+        answer.append(i[0][0])
+        answer.append(i[1][0])
 
 print(answer)
